839bf3d3b573e901f357d22e201a90416bf97572
Kayky de Brito <oss@kaykyb.com>
2024-08-23 03:31:41 -0300

    Improve rune parsing test

--- lexer/utils_test.go
+++ lexer/utils_test.go
@@ -1,92 +1,107 @@
-package lexer
+package lexer_test
 
 import (
 	"testing"
+
+	"isigo/lexer"
+	"isigo/syntax"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestIsWhitespaceRune(t *testing.T) {
-	assert.True(t, IsWhitespaceRune(' '))
-	assert.False(t, IsWhitespaceRune('\r'))
+	assert.True(t, lexer.IsWhitespaceRune(' '))
+	assert.False(t, lexer.IsWhitespaceRune('a'))
 }
 
 func TestIsNewLineRune(t *testing.T) {
-	assert.True(t, IsNewLineRune('\n'))
-	assert.False(t, IsNewLineRune('\r'))
+	assert.True(t, lexer.IsNewLineRune('\n'))
+	assert.False(t, lexer.IsNewLineRune('a'))
 }
 
 func TestIsTabRune(t *testing.T) {
-	assert.True(t, IsTabRune('\t'))
-	assert.False(t, IsTabRune('\r'))
+	assert.True(t, lexer.IsTabRune('\t'))
+	assert.False(t, lexer.IsTabRune('a'))
 }
 
-func TestIsCartridgeReturn(t *testing.T) {
-	assert.True(t, IsCartridgeReturnRune('\r'))
-	assert.False(t, IsCartridgeReturnRune('\n'))
+func TestIsCartridgeReturnRune(t *testing.T) {
+	assert.True(t, lexer.IsCartridgeReturnRune('\r'))
+	assert.False(t, lexer.IsCartridgeReturnRune('a'))
 }
 
 func TestIsOperatorRune(t *testing.T) {
-	assert.True(t, IsOperatorRune('+'))
-	assert.True(t, IsOperatorRune('-'))
-	assert.True(t, IsOperatorRune('*'))
-	assert.True(t, IsOperatorRune('/'))
-	assert.True(t, IsOperatorRune('<'))
-	assert.True(t, IsOperatorRune('>'))
-	assert.True(t, IsOperatorRune('!'))
-	assert.True(t, IsOperatorRune('='))
-
-	assert.False(t, IsOperatorRune('a'))
-	assert.False(t, IsOperatorRune('@'))
-	assert.False(t, IsOperatorRune('`'))
-	assert.False(t, IsOperatorRune('m'))
-	assert.False(t, IsOperatorRune('n'))
-	assert.False(t, IsOperatorRune('x'))
-	assert.False(t, IsOperatorRune('y'))
-	assert.False(t, IsOperatorRune(':'))
+	assert.True(t, lexer.IsOperatorRune('+'))
+	assert.True(t, lexer.IsOperatorRune('='))
+	assert.False(t, lexer.IsOperatorRune('&'))
 }
 
 func TestIsLetterRune(t *testing.T) {
-	assert.True(t, IsLetterRune('a'))
-	assert.True(t, IsLetterRune('b'))
-	assert.True(t, IsLetterRune('z'))
-
-	assert.True(t, IsLetterRune('A'))
-	assert.True(t, IsLetterRune('B'))
-	assert.True(t, IsLetterRune('Z'))
-
-	assert.False(t, IsLetterRune('1'))
-	assert.False(t, IsLetterRune('2'))
-	assert.False(t, IsLetterRune('0'))
+	assert.True(t, lexer.IsLetterRune('a'))
+	assert.True(t, lexer.IsLetterRune('Z'))
+	assert.False(t, lexer.IsLetterRune('1'))
+	assert.False(t, lexer.IsLetterRune('@'))
 }
 
 func TestIsDigitRune(t *testing.T) {
-	assert.True(t, IsDigitRune('0'))
-	assert.True(t, IsDigitRune('1'))
-	assert.True(t, IsDigitRune('2'))
-	assert.True(t, IsDigitRune('3'))
-	assert.True(t, IsDigitRune('4'))
-	assert.True(t, IsDigitRune('5'))
-	assert.True(t, IsDigitRune('6'))
-	assert.True(t, IsDigitRune('7'))
-	assert.True(t, IsDigitRune('8'))
-	assert.True(t, IsDigitRune('9'))
-
-	assert.False(t, IsDigitRune('a'))
-	assert.False(t, IsDigitRune('/'))
-	assert.False(t, IsDigitRune('z'))
-	assert.False(t, IsDigitRune('A'))
-	assert.False(t, IsDigitRune('*'))
-	assert.False(t, IsDigitRune('Z'))
+	assert.True(t, lexer.IsDigitRune('0'))
+	assert.True(t, lexer.IsDigitRune('9'))
+	assert.False(t, lexer.IsDigitRune('a'))
+	assert.False(t, lexer.IsDigitRune('-'))
 }
 
 func TestIsDecimalSeparator(t *testing.T) {
-	assert.True(t, IsDecimalSeparator('.'))
-	assert.False(t, IsDecimalSeparator(','))
+	assert.True(t, lexer.IsDecimalSeparator('.'))
+	assert.False(t, lexer.IsDecimalSeparator(','))
 }
 
 func TestIsStatementTerminator(t *testing.T) {
-	assert.True(t, IsStatementTerminator('.'))
-	assert.False(t, IsStatementTerminator(','))
-	assert.False(t, IsStatementTerminator(';'))
+	assert.True(t, lexer.IsStatementTerminator('.'))
+	assert.False(t, lexer.IsStatementTerminator(';'))
+}
+
+func TestIsColon(t *testing.T) {
+	assert.True(t, lexer.IsColon(':'))
+	assert.False(t, lexer.IsColon(';'))
+}
+
+func TestIsOpenParenthesis(t *testing.T) {
+	assert.True(t, lexer.IsOpenParenthesis('('))
+	assert.False(t, lexer.IsOpenParenthesis(')'))
+}
+
+func TestIsCloseParenthesis(t *testing.T) {
+	assert.True(t, lexer.IsCloseParenthesis(')'))
+	assert.False(t, lexer.IsCloseParenthesis('('))
+}
+
+func TestIsOpenBrace(t *testing.T) {
+	assert.True(t, lexer.IsOpenBrace('{'))
+	assert.False(t, lexer.IsOpenBrace('}'))
+}
+
+func TestIsCloseBrace(t *testing.T) {
+	assert.True(t, lexer.IsCloseBrace('}'))
+	assert.False(t, lexer.IsCloseBrace('{'))
+}
+
+func TestIsComma(t *testing.T) {
+	assert.True(t, lexer.IsComma(','))
+	assert.False(t, lexer.IsComma('.'))
+}
+
+func TestIsStringDelimiter(t *testing.T) {
+	assert.True(t, lexer.IsStringDelimiter('"'))
+	assert.False(t, lexer.IsStringDelimiter('\''))
+}
+
+func TestIsReservedWord(t *testing.T) {
+	assert.True(t, lexer.IsReservedWord(syntax.Program))
+	assert.True(t, lexer.IsReservedWord(syntax.Read))
+	assert.False(t, lexer.IsReservedWord("customWord"))
+}
+
+func TestIsTypeT(t *testing.T) {
+	assert.True(t, lexer.IsTypeT(syntax.IntegerT))
+	assert.True(t, lexer.IsTypeT(syntax.FloatT))
+	assert.False(t, lexer.IsTypeT("customType"))
 }
